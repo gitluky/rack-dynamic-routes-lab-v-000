9ba75a33cf5fd5dd14a6f6f7d832f589e3a2581e
Jonathan Luk <jluk87@gmail.com>
2018-08-25 15:52:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    binding.pry
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item_found = Items.all.detect {|item| item.name == item_name}
+      resp.write item_found.price
   end
 
 end
