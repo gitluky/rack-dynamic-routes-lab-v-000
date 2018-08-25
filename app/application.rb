1ccc0ce031084e079ed8e6b936f09a83bfe60cb1
Jonathan Luk <jluk87@gmail.com>
2018-08-25 15:54:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,15 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item_found = Items.all.detect {|item| item.name == item_name}
+    end
+    if item_found
       resp.write item_found.price
+    else
+      resp.write "No item found."
+      resp.status 404
+    end
+
+
   end
 
 end
