7e7c5c7a19c3a042c027f563b0d2ea6627a5ad5d
Jonathan Luk <jluk87@gmail.com>
2018-08-25 15:45:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    binding.pry
     if req.path.include?("/items/")
       item_name = req.path.gsub("/+items/", "")
     end
